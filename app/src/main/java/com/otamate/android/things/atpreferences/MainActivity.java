c763d03a14ac15414268c0a5bf9ddba7c9376252
otamate <carl.whalley@otamate.com>
2017-08-30 18:46:18 +0100

    Minor layout tweaks

--- app/src/main/java/com/otamate/android/things/atpreferences/MainActivity.java
+++ app/src/main/java/com/otamate/android/things/atpreferences/MainActivity.java
@@ -1,12 +1,11 @@
 package com.otamate.android.things.atpreferences;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
